4451b02a1e97f6622a31bd34a957fd164c30a374
Aaron Bustard <a.bustard@gmail.com>
2016-11-13 12:10:59 -0800

    rmd2md defaults changed

--- rmd2md.R
+++ rmd2md.R
@@ -15,7 +15,7 @@
 #' @author Jason Bryer <jason@bryer.org> edited by Andy South
 rmd2md <- function( path_site = getwd(),
                     dir_rmd = "_Rmd",
-                    dir_md = "_md",                              
+                    dir_md = "_posts",                              
                     #dir_images = "figures",
                     url_images = "figures/",
                     out_ext='.md', 
